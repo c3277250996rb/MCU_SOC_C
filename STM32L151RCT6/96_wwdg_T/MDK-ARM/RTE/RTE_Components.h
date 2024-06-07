336177c8cf4602066f75cd6070e5307460b84daf
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2024-06-07 15:17:01 +0800

    96_wwdg_T

---
+++ STM32L151RCT6/96_wwdg_T/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '96_wwdg_T' 
+ * Target:  '96_wwdg_T' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l1xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
